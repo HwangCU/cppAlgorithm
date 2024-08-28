52f1d8f711a918fb847d83b7dfe99f6a6b68fd0d
HwangChiUn <hcun1004@naver.com>
2024-08-28 21:11:16 +0900

    20240828

---
+++ ChiChi/PS/SWEA/2115. [모의 SW 역량테스트] 벌꿀채취/code.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <
+using namespace std;
+
+int N,M,C;
+int map[11][11];
+struct Point
+{
+    int y;
+    int x;
+};
+
+void input()
+{
+    cin>>N>>M>>C;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin>>map[i][j];
+        }
+    }
+}
+
+void dfs(Point st)
+{
+    for (int i = 0; i <= N-M; i++)
+    {
+        /* code */
+    }
+    
+}
+
+void solve()
+{
+    for (int i = 0; i <= N-M; i++)
+    {
+        for (int j = 0; j < count; j++)
+        {
+        
+        }
+        
+    }
+    
+}
+int main()
+{
+    int T;
+    cin>>T;
+    for (int t = 1; t <= T; t++)
+    {
+
+    }
+    
+}
