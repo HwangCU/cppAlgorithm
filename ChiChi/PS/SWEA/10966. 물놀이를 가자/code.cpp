ee17767190e7e5816063bd00b72b07d339e8ebbd
HwangChiUn <hcun1004@naver.com>
2024-09-02 22:21:09 +0900

    20240902

--- ChiChi/PS/SWEA/10966. 물놀이를 가자/code.cpp
+++ ChiChi/PS/SWEA/10966. 물놀이를 가자/code.cpp
@@ -1,110 +1,65 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <cstring>
+#include<iostream>
+#include<vector>
+#include<queue>
 using namespace std;
 
 int N, M;
-char map[1001][1001];
-int visited[1001][1001];
-int costMap[1001][1001];
-int dy[] = { -1,1,0,0 };
-int dx[] = { 0,0,-1,1 };
-int ans = 0;
+int map[1001][1001];
 
-struct Point
-{
-	int y, x;
-};
-
-void init()
-{
-	memset(map, 0, sizeof(map));
-	memset(visited, 0, sizeof(visited));
-	memset(costMap, 0, sizeof(costMap));
-	ans = 0;
-}
-
-void input()
-{
-	cin >> N >> M;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			cin >> map[i][j];
-		}
-	}
-}
-
-void print()
-{
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			cout << visited[i][j] << ' ';
-		}
-		cout << '\n';
-	}
-	cout << '\n';
-}
-
-void bfs()
-{
-	queue<Point> q;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			if (map[i][j] == 'W')
-			{
-				q.push({i,j});
-			}
-		}
-	}
-
-	while (!q.empty())
-	{
-		Point now = q.front();
-		q.pop();
-		print();
-		for (int d = 0; d < 4; d++)
-		{
-			Point next = { now.y + dy[d], now.x + dx[d] };
-			if (next.y < 0 || next.y >= N || next.x < 0 || next.x >= M) continue;
-			if (map[next.y][next.x] == 'W') continue;
-			if (visited[next.y][next.x] > visited[now.y][now.x] + 1) continue;
-
-			visited[next.y][next.x] = visited[now.y][now.x] + 1;
-			q.push({ next.y, next.x });
-		}
-	}
-}
-
-void solve()
-{
-	bfs();
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			ans += visited[i][j];
-		}
-	}
-}
+int dx[] = { 1,0,-1,0 }; //우하좌상
+int dy[] = { 0,1,0,-1 };
 
 int main()
 {
-	freopen("input.txt", "r", stdin);
-	int T;
-	cin >> T;
-	for (int t = 1; t <= T; t++)
-	{
-		init();
-		input();
-		solve();
-		cout << '#' << t << ' ' << ans << '\n';
-	}
+    ios::sync_with_stdio(false);
+
+
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    //cin.tie(0); 17,18번라인은 같다
+
+
+    for (int t = 0; t < T; ++t)
+    {
+        int result = 0;
+        cin >> N >> M;
+        queue<pair<int, int>> q;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                char ch;
+                cin >> ch;
+                if (ch == 'W') {
+                    map[i][j] = 0;
+                    q.push({ i,j });
+                }
+                else
+                    map[i][j] = -1;
+            }
+        }
+
+        while (!q.empty()) {
+            int cx = q.front().first;
+            int cy = q.front().second;
+            q.pop();
+
+            for (int d = 0; d < 4; d++) {
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+
+                if ((nx >= 0 && nx < N) && (ny >= 0 && ny < M)) {
+                    if (map[nx][ny] == -1) {
+                        map[nx][ny] = map[cx][cy] + 1;
+                        result += map[nx][ny];
+                        q.push({ nx,ny });
+                    }
+                }
+            }
+        }
+        cout << "#" << t + 1 << ' ' << result << '\n';
+
+        // '\n' vs endl 입력버퍼, 출력버퍼를 비우냐 마냐
+
+    }
+
 }
